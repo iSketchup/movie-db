f3898961c9bfc9459f62499347d9785c5b58d40a
luis.kaufmann@student.htl-rankweil.at <luis.kaufmann@student.htl-rankweil.at>
2026-03-18 20:22:57 +0000

    Edited Studio and Backend

--- client_code/Studio/__init__.py
+++ client_code/Studio/__init__.py
@@ -5,17 +5,48 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Studio(StudioTemplate):
-  def __init__(self, sid,  **properties):
-    # Set Form properties and Data Bindings.
+  def __init__(self, sid, **properties):
     self.init_components(**properties)
-
     self.RP_Movies.items = anvil.server.call('query_database_dict_Movie', sid)
-  
-    # Any code you write here will run before the form opens.
+    self.load_ratings_chart(sid)
 
   @handle("lk_Homepage", "click")
   def lk_Homepage_click(self, **event_args):
-    """This method is called when the link is clicked"""
     open_form('Homepage')
+
+  def load_ratings_chart(self, studio_id):
+    data = anvil.server.call('query_ratings_by_studio', studio_id)
+
+    titles = [row['Title'] for row in data]
+    ratings = [row['AvgRating'] for row in data]
+
+    self.plot_1.data = [{
+      'type': 'bar',
+      'orientation': 'h',
+      'x': ratings,
+      'y': titles,
+      'marker': {'color': '#4A90E2'},
+      'text': [f"{r:.1f}" for r in ratings],
+      'textposition': 'outside'
+        }]
+        
+    self.plot_1.layout = {
+      'xaxis': {
+        'range': [0, 10],
+        'title': 'Ø Rating',
+        'color': '#ffffff',
+        'gridcolor': '#444444',
+        'zerolinecolor': '#444444'
+      },
+      'yaxis': {
+        'automargin': True,
+        'color': '#ffffff',
+        'gridcolor': '#444444'
+      },
+      'paper_bgcolor': 'transparent',
+      'plot_bgcolor': 'transparent',
+      'font': {'color': '#ffffff'},
+      'margin': {'l': 150, 'r': 40, 't': 20, 'b': 40},
+      'height': max(200, len(titles) * 50)
+    }
--- server_code/Backend.py
+++ server_code/Backend.py
@@ -16,8 +16,9 @@ def query_database_dict_All_Movies():
     result = cur.execute(query).fetchall()
   return [dict(row) for row in result]
 
+
 @anvil.server.callable
-def query_database_dict_Ratings(id:int):
+def query_database_dict_Ratings(id: int):
   query = f'SELECT * FROM RATING WHERE MID={id}'
   with sqlite3.connect(data_files["movies.db"]) as conn:
     conn.row_factory = sqlite3.Row
@@ -25,17 +26,25 @@ def query_database_dict_Ratings(id:int):
     result = cur.execute(query).fetchall()
   return [dict(row) for row in result]
 
+
 @anvil.server.callable
-def query_database_dict_Actors(id:int):
-  query = f'SELECT * FROM ACTOR WHERE MID={id}'
+def query_database_dict_Actors(id: int):
+  """Returns all actors for a given movie MID, via MOVIE_ACTOR junction table."""
+  query = f'''
+        SELECT ACTOR.*
+        FROM ACTOR
+        JOIN MOVIE_ACTOR ON ACTOR.AID = MOVIE_ACTOR.AID
+        WHERE MOVIE_ACTOR.MID = {id}
+    '''
   with sqlite3.connect(data_files["movies.db"]) as conn:
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
   return [dict(row) for row in result]
 
+
 @anvil.server.callable
-def query_database_dict_Success(id:int):
+def query_database_dict_Success(id: int):
   query = f'SELECT * FROM Success WHERE MID={id}'
   with sqlite3.connect(data_files["movies.db"]) as conn:
     conn.row_factory = sqlite3.Row
@@ -43,31 +52,64 @@ def query_database_dict_Success(id:int):
     result = cur.execute(query).fetchall()
   return [dict(row) for row in result]
 
-@anvil.server.callable
-def query_database_dict_Studio(id:int):
-  query = f'SELECT * FROM STUDIO WHERE MID={id}'
-  with sqlite3.connect(data_files["movies.db"]) as conn:
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    result = cur.execute(query).fetchall()
-  return [dict(row) for row in result]
 
 @anvil.server.callable
-def query_database_dict_Genre(id:int):
-  query = f'SELECT * FROM GENRE WHERE MID={id}'
-  with sqlite3.connect(data_files["movies.db"]) as conn:
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    result = cur.execute(query).fetchall()
-  return [dict(row) for row in result]
-
-
-@anvil.server.callable
-def query_database_dict_Movie(sid:int):
+def query_database_dict_Studio(id: int):
+  """Returns all studios for a given movie MID, via MOVIE_STUDIO junction table."""
   query = f'''
-        SELECT MOVIE.* FROM MOVIE 
-        JOIN STUDIO ON MOVIE.MID = STUDIO.MID 
-        WHERE STUDIO.SID = {sid}
+        SELECT STUDIO.*
+        FROM STUDIO
+        JOIN MOVIE_STUDIO ON STUDIO.SID = MOVIE_STUDIO.SID
+        WHERE MOVIE_STUDIO.MID = {id}
+    '''
+  with sqlite3.connect(data_files["movies.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
+
+
+@anvil.server.callable
+def query_database_dict_Genre(id: int):
+  """Returns all genres for a given movie MID, via MOVIE_GENRE junction table."""
+  query = f'''
+        SELECT GENRE.*
+        FROM GENRE
+        JOIN MOVIE_GENRE ON GENRE.GID = MOVIE_GENRE.GID
+        WHERE MOVIE_GENRE.MID = {id}
+    '''
+  with sqlite3.connect(data_files["movies.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
+
+
+@anvil.server.callable
+def query_database_dict_Movie(sid: int):
+  """Returns all movies for a given studio SID, via MOVIE_STUDIO junction table."""
+  query = f'''
+        SELECT MOVIE.*
+        FROM MOVIE
+        JOIN MOVIE_STUDIO ON MOVIE.MID = MOVIE_STUDIO.MID
+        WHERE MOVIE_STUDIO.SID = {sid}
+    '''
+  with sqlite3.connect(data_files["movies.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
+
+@anvil.server.callable
+def query_ratings_by_studio(sid: int):
+  query = f'''
+        SELECT MOVIE.Title, AVG(RATING.rating) as AvgRating
+        FROM MOVIE
+        JOIN MOVIE_STUDIO ON MOVIE.MID = MOVIE_STUDIO.MID
+        JOIN RATING ON MOVIE.MID = RATING.MID
+        WHERE MOVIE_STUDIO.SID = {sid}
+        GROUP BY MOVIE.MID, MOVIE.Title
+        ORDER BY AvgRating DESC
     '''
   with sqlite3.connect(data_files["movies.db"]) as conn:
     conn.row_factory = sqlite3.Row
